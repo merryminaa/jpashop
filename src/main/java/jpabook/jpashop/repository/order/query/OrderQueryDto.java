931901decd0bdce689ee82ea889d69ad18238dc1
Mina Park <kyminp@gmail.com>
2022-10-02 18:23:44 +0900

    add:컬렉션 주문조회(DTO로 직접 조회,플랫데이터 최적화)

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -5,11 +5,13 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId") //groupby 할 때 묶어주는 기능 명시
 public class OrderQueryDto {
     private Long orderId;
     private String name;
@@ -24,9 +26,18 @@ public class OrderQueryDto {
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
-//        this.orderItems = orderItems; //컬렉션은 생성자로 가져와서 받을 수 없음
     }
 
     public OrderQueryDto() {
+    }
+
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
     }
 }
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -75,4 +75,17 @@ public class OrderQueryRepository {
     private static List<Long> toOrderIds(List<OrderQueryDto> result) {
         return result.stream().map(o -> o.getOrderId()).collect(Collectors.toList());
     }
+
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery("select new" +
+                " jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                " from Order o" +
+                " join o.member m" +
+                " join o.delivery d" +
+                " join o.orderItems oi" +
+                " join oi.item i", OrderFlatDto.class)
+                .getResultList();
+        //쿼리 1회에 모든 필요 데이터 조회(장점)
+
+    }
 }
